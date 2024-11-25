99932b15d385e7671f8be7e0de0f2dea8e2f8362
JimmyMaina-Cybersec <jimmy.maina.cybersec@gmail.com>
2024-11-25 11:06:30 +0300

    chore(script modification): Refined the scripts output layout

--- Blockchain/Scripts/minikube.sh
+++ Blockchain/Scripts/minikube.sh
@@ -11,9 +11,10 @@ fi
 MINIKUBE_IP=$(minikube ip)
 
 # Update /etc/hosts for peer0 and orderer0
+echo -e "\nUpdating /etc/hosts..."
 sudo sed -i '/pesachain.local/d' /etc/hosts
 sudo sed -i '/orderer.local/d' /etc/hosts
-echo "$MINIKUBE_IP   peer0.local" | sudo tee -a /etc/hosts
-echo "$MINIKUBE_IP   orderer0.local" | sudo tee -a /etc/hosts
+echo "$MINIKUBE_IP   pesachain.local" | sudo tee -a /etc/hosts
+echo "$MINIKUBE_IP   orderer.local" | sudo tee -a /etc/hosts
 
-echo "Minikube IP ($MINIKUBE_IP) has been added to /etc/hosts with the domains 'peer0.local' and 'orderer0.local'"
+echo -e "\nMinikube IP ($MINIKUBE_IP) has been added to /etc/hosts with the domains 'pesachain.local' and 'orderer.local'"
